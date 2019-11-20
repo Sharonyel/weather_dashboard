dae47e984051621d698670b3127ef9ef307c3a07
Sharon Yelverton <ladysharonyel@me.com>
2019-11-20 15:49:37 -0500

    css style

---
+++ assets/moments.js
@@ -0,0 +1,18 @@
+
+var update = function () {
+
+    // get the current day
+    var currentDay = document.querySelector("#currDay")
+        .innerHTML = moment().format("MM/DD/YYYY");
+   
+
+    }
+    
+    setInterval(update, 1000);
+    
+    
+    
+    
+    
+    
+    
